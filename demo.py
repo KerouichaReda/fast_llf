d43f5e0b67c774a4ad21e6187d297560ffca0e45
Reda Kerouicha <kerouicha.reda@gmail.com>
2020-01-24 20:26:25 +0800

    clearing the code

--- demo.py
+++ demo.py
@@ -3,37 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import llf
+
 np.set_printoptions(precision=5)
 np.set_printoptions(suppress=True)
-im= cv.imread('jacek.jpg')
+
+im= cv.imread('sjacek.jpg')
 imrgb=cv.cvtColor(im,cv.COLOR_BGR2RGB)/255
 img=cv.cvtColor(im,cv.COLOR_BGR2GRAY)/255
 img32=np.float32(img)
 I=img32
+
 I_ratio=imrgb/llf.repeat(img)
-r=328
-c=492
-row=np.arange(0,r,2)
-col=np.arange(0,c,2)      
-row_r,col_c=np.meshgrid(col,row) 
-#gau=llf.gaussian_pyramid(img,None,None)
-#lap=llf.laplacian_pyramid(img,None,None)
 
 sigma=0.3
 N=10
-fact=-0.5
+fact=0.5
 t = time.time()
-# do stuff
+#Filtering
 
 im_e=llf.llf(img,sigma,fact,N)
 im_ergb=llf.repeat(im_e)*I_ratio
 elapsed = time.time() - t
 print(elapsed)
+#plot the image
 plt.subplot(1,2,1)
 imgplt=plt.imshow(imrgb)
 plt.subplot(1,2,2)
 imgplt=plt.imshow(im_ergb)
 plt.show()
 wrt=cv.cvtColor(np.float32(im_ergb*255),cv.COLOR_RGB2BGR)
-cv.imwrite('jacek_smooth.jpg',wrt)
-#
